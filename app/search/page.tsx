f3584630949c05513cb31f536de01407c05178b5
Lucifersql <65667464+ramdanolii14@users.noreply.github.com>
2025-07-25 03:35:25 +0800

    Update page.tsx

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,13 +1,17 @@
 import { Suspense } from "react";
-import dynamicImport from "next/dynamic";
+import dynamic from "next/dynamic";
 
-const SearchClient = dynamicImport(() => import("./SearchClient"), {
+const SearchClient = dynamic(() => import("./SearchClient"), {
   ssr: false,
 });
 
+export const dynamicParams = true;
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
+
 export default function Page() {
   return (
-    <Suspense fallback={<p className="mt-20 text-center">Loading...</p>}>
+    <Suspense fallback={<p className="p-4 text-center">Loading...</p>}>
       <SearchClient />
     </Suspense>
   );
