79612fa34adc53de0faf435d3a3c15e54f90088e
Lucifersql <65667464+ramdanolii14@users.noreply.github.com>
2025-07-25 03:21:42 +0800

    Update page.tsx

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { supabase } from "@/supabase/client";
