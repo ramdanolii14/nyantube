a03b8f33ec05b65f1ba7a96d846204954528960d
Lucifersql <65667464+ramdanolii14@users.noreply.github.com>
2025-07-28 23:44:16 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,13 +8,45 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "txtcdwwrpusmmbvoimkq.supabase.co", // kalau ada avatar dari supabase
+        hostname: "txtcdwwrpusmmbvoimkq.supabase.co",
       },
       {
         protocol: "https",
         hostname: "rais2gather.fun",
       },
     ],
+  },
+  async headers() {
+    const securityHeaders = [
+      {
+        key: 'Content-Security-Policy',
+        value:
+          "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src * blob: data:; media-src *; connect-src *",
+      },
+      {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+      },
+      {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN',
+      },
+      {
+        key: 'Referrer-Policy',
+        value: 'strict-origin-when-cross-origin',
+      },
+      {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=()',
+      },
+    ];
+
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
