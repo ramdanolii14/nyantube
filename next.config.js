48b8e03d6ee4e19a9d4cc0c2c4328ad0fd35fa32
Lucifersql <65667464+ramdanolii14@users.noreply.github.com>
2025-07-28 23:46:42 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,34 +17,28 @@ const nextConfig = {
     ],
   },
   async headers() {
-    const securityHeaders = [
-      {
-        key: 'Content-Security-Policy',
-        value:
-          "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src * blob: data:; media-src *; connect-src *",
-      },
-      {
-        key: 'X-Content-Type-Options',
-        value: 'nosniff',
-      },
-      {
-        key: 'X-Frame-Options',
-        value: 'SAMEORIGIN',
-      },
-      {
-        key: 'Referrer-Policy',
-        value: 'strict-origin-when-cross-origin',
-      },
-      {
-        key: 'Permissions-Policy',
-        value: 'camera=(), microphone=(), geolocation=()',
-      },
-    ];
-
     return [
       {
-        source: '/(.*)',
-        headers: securityHeaders,
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+          },
+        ],
       },
     ];
   },
