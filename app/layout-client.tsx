0d43dd62ba6dcaa8d9079736350c0594a8130329
Lucifersql <65667464+ramdanolii14@users.noreply.github.com>
2025-07-29 01:38:14 +0800

    Create layout-client.tsx

---
+++ app/layout-client.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+export default function LayoutClient({ children }: { children: React.ReactNode }) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  return <SessionContextProvider supabaseClient={supabaseClient}>{children}</SessionContextProvider>;
+}
