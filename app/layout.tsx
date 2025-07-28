54c5bae85402dbba85e50d740d5d2ff4dcaebecf
Lucifersql <65667464+ramdanolii14@users.noreply.github.com>
2025-07-29 01:46:02 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,23 @@ import "./globals.css";
 import Navbar from "@/app/components/Navbar";
 import LayoutClient from "@/app/layout-client";
 
-export const metadata = { title: "Nyantube", description: "Streaming Online..." };
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+const supabase = createBrowserSupabaseClient();
+
+export const metadata = {
+  title: "NyanTube",
+  description: "Streaming Video Online Bersama NyanTube",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-  <SessionContextProvider supabaseClient={supabase}>
-    <html lang="en">
-        <head><script src="https://www.google.com/recaptcha/api.js" async defer /></head>
+    <SessionContextProvider supabaseClient={supabase}>
+      <html lang="en">
+        <head>
+          <script src="https://www.google.com/recaptcha/api.js" async defer />
+        </head>
         <body className="bg-gray-100">
           <LayoutClient>
             <Navbar />
