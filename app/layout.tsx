62643d877b8c74a35be53a90e9b5f904781943d6
Lucifersql <65667464+ramdanolii14@users.noreply.github.com>
2025-07-29 01:43:28 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,14 +6,16 @@ export const metadata = { title: "Nyantube", description: "Streaming Online..." 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
+  <SessionContextProvider supabaseClient={supabase}>
     <html lang="en">
-      <head><script src="https://www.google.com/recaptcha/api.js" async defer /></head>
-      <body className="bg-gray-100">
-        <LayoutClient>
-          <Navbar />
-          {children}
-        </LayoutClient>
-      </body>
-    </html>
+        <head><script src="https://www.google.com/recaptcha/api.js" async defer /></head>
+        <body className="bg-gray-100">
+          <LayoutClient>
+            <Navbar />
+            {children}
+          </LayoutClient>
+        </body>
+      </html>
+    </SessionContextProvider>
   );
 }
