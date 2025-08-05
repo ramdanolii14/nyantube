09bcd0f32fb171990a93e263d0e61349d2a46ee3
Lucifersql <65667464+ramdanolii14@users.noreply.github.com>
2025-08-05 08:14:50 +0800

    Update route.ts

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,14 +1,24 @@
+// app/sitemap.xml/route.ts
 import { NextResponse } from "next/server";
+
+export const revalidate = 60 * 60; // update tiap 1 jam
 
 export async function GET() {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://nyantube.ramdan.fun";
-  const now = new Date().toISOString();
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
+    <loc>${baseUrl}/main-sitemap.xml</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </sitemap>
+  <sitemap>
     <loc>${baseUrl}/video-sitemap.xml</loc>
-    <lastmod>${now}</lastmod>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${baseUrl}/channel-sitemap.xml</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
   </sitemap>
 </sitemapindex>`;
 
