933bf1ee50a1d4697fd1677ecb277ef8a6479394
Lucifersql <65667464+ramdanolii14@users.noreply.github.com>
2025-08-15 09:35:19 +0800

    Create page.tsx

---
+++ app/status/page.tsx
@@ -0,0 +1,2 @@
+<script type="text/javascript" src="https://static.hetrix.io/iframeResizer/iframeResizer.min.js"></script>
+<iframe class="htframe" src="https://wl.hetrixtools.com/r/e33f3d15f1d8738ee74760c132ea972e/" width="100%" scrolling="no" style="border:none;" sandbox="allow-scripts allow-same-origin allow-popups" onload="iFrameResize([{log:false}],'.htframe')"></iframe>
