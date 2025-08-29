635e05606286d93e7b62e787c5faf4fbb7826ac3
Lucifersql <65667464+ramdanolii14@users.noreply.github.com>
2025-08-30 00:50:19 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,16 @@ import Navbar from "@/app/components/Navbar";
 
 export const metadata = {
   title: "NyanStream - Online Video Sharing For Everyone",
-  description: "Streaming Online Tanpa Batas Bersama NyanTube. Powerfull video platform, only for Sigma. Share your moment here, make memories with us. We all are like family here.",
+  description:
+    "Streaming Online Tanpa Batas Bersama NyanTube. Powerfull video platform, only for Sigma. Share your moment here, make memories with us. We all are like family here.",
+  alternates: {
+    canonical: "https://nyanstream.my.id/", //update canonical huhuhu capek jir
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-      </head>
       <body className="bg-gray-100">
         <Navbar />
         {children}
