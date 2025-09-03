fcc16a5f2d0fd547074b5ad61b9c16b0ee4c2d24
Lucifersql <65667464+ramdanolii14@users.noreply.github.com>
2025-09-04 01:46:35 +0800

    Update route.ts

--- app/api/get-ip/route.ts
+++ app/api/get-ip/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const ip =
+    req.headers.get("cf-connecting-ip") ||
     req.headers.get("x-forwarded-for")?.split(",")[0] ||
     req.headers.get("x-real-ip") ||
     req.ip ||
