1848662b7a03db594921d8892bb7cf82b56c824c
Lucifersql <65667464+ramdanolii14@users.noreply.github.com>
2025-11-29 13:05:02 +0800

    Update page.tsx

--- app/status/page.tsx
+++ app/status/page.tsx
@@ -13,12 +13,25 @@ export default function StatusPage() {
   return (
     <div className="bg-white min-h-screen p-5">
       {/* Load script resmi dari Hetrix */}
-      <script type="text/javascript" 
-      src="https://static.hetrix.io/iframeResizer/iframeResizer.min.js"></script>
-
+      <Script
+        src="https://static.hetrix.io/iframeResizer/iframeResizer.min.js"
+        strategy="afterInteractive"
+      />
+      
       {/* Iframe Hetrix */}
-      <iframe 
-      class="htframe" src="https://wl.hetrixtools.com/report/uptime/517d4466a9f65703fc2dbb649c10d7e5/" width="100%" scrolling="no" style="border:none;" sandbox="allow-scripts allow-same-origin allow-popups" onload="iFrameResize([{log:false}],'.htframe')"></iframe>
+      <iframe
+        className="htframe"
+        src="https://wl.hetrixtools.com/report/uptime/517d4466a9f65703fc2dbb649c10d7e5/"
+        width="100%"
+        scrolling="no"
+        style={{ border: "none" }}
+        sandbox="allow-scripts allow-same-origin allow-popups"
+        onLoad={() => {
+          if (typeof window !== "undefined" && (window as any).iFrameResize) {
+            (window as any).iFrameResize([{ log: false }], ".htframe");
+          }
+        }}
+      ></iframe>
     </div>
   );
 }
